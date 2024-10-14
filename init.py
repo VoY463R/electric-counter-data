a88b5963292755d4601120aa8782a1611e21c91a
Kamil Tylka <kamiltylka@interia.pl>
2024-10-14 18:31:26 +0200

    Stworzenie opcji logowania do strony

--- init.py
+++ init.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 Bootstrap5(app)
+app.config['RECAPTCHA_PUBLIC_KEY'] = "6LcLVmEqAAAAAEMAeioWuCCppHqLuMRK4drkcbTx"
+app.config['RECAPTCHA_PRIVATE_KEY'] = "6LcLVmEqAAAAAN4gcOy7OFDLJxwdGL8Ge5qTrAIB"
+app.config["SECRET_KEY"] = "163*%$uSfJLG^E"
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired("A username is required!")])
+    password = PasswordField("Password",validators=[InputRequired("A password is required!")])
+    # recaptcha = RecaptchaField()
+    
+@app.route('/home', methods=["POST", "GET"])
 def home():
-    return "Hello"
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        return "Hello there"
+    return render_template("index.html", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
